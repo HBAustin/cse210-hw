6c08a9c21697ad1d82d51e1cc30c138568534451
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-06-29 12:30:31 +0200

    Completed "Learning04"

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assign1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("Robert Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeWorkList());
+
+        WritingAssignment assign3 = new WritingAssignment("Mary Water", "European History", "The Causes of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
     }
 }
